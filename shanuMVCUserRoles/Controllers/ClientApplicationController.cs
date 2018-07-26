81bb4211ab179a4b2dcdd177e875698d131f3441
BrettMos <36499837+BrettMos@users.noreply.github.com>
2018-07-26 15:20:34 +0200

    initial commit

---
+++ shanuMVCUserRoles/Controllers/ClientApplicationController.cs
@@ -0,0 +1,44 @@
+﻿using shanuMVCUserRoles.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+/*namespace shanuMVCUserRoles.Controllers
+{
+    public class ClientApplicationController : Controller
+    {
+        // GET: ClientApplication
+        public ActionResult Index()
+        {
+            ClientApplicationEntity DB = new Models.ClientApplicationEntity();
+            Client_Application ClientApp = DB.Client_Application;
+            return View(ClientAppList);
+        }
+
+    }
+}*/
+namespace shanuMVCUserRoles.Controllers
+{
+    public class ClientApplicationController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Client_Application obj)
+
+        {
+            if (ModelState.IsValid)
+            {
+                Client_Application db = new Client_Application();
+            }
+            return View(obj);
+        }
+    }
+}
